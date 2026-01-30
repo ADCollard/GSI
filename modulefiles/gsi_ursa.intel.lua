5903fb1a93bc32af98e0a5b1a58b68afbb84ebdc
Andrew Collard <andrew.collard@noaa.gov>
2026-01-30 22:04:42 +0000

    Changes required to compile with spack stack 2.0.0

--- modulefiles/gsi_ursa.intel.lua
+++ modulefiles/gsi_ursa.intel.lua
@@ -1,19 +1,17 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-2.0.0/envs/ue-oneapi-2025.2.1/modules/Core")
 
-local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2025.2.1"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
-local oneapi_mkl_ver=os.getenv("oneapi_mkl_ver") or "2024.2.1"
-local stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
-local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
-local crtm_fix_ver=os.getenv("crtm_fix_ver") or "2.4.0.2"
+local oneapi_mkl_ver=os.getenv("oneapi_mkl_ver") or "2025.2.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.31.8"
+local crtm_fix_ver=os.getenv("crtm_fix_ver") or "3.1.2"
 
-load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-compilers", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 load(pathJoin("intel-oneapi-mkl", oneapi_mkl_ver))
-load(pathJoin("stack-python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
